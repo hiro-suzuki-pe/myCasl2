02504ce4c1ac1942f61077672f2544719f8bced7
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-08-29 05:09:52 +0900

    動き出したが期待通りにダンプしてない。デバッグを追加。

--- xcd/xcd.c
+++ xcd/xcd.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 
-/* 
- * xcd --- hexa and char dump
- */
-int xcd(char *file_name)
-{
-    if (file_name == NULL)
-        printf("xcd()\n");
-    else{
-        /*
-                FILE *fp = fopen(file_name, "r");
-                if (fp == NULL){
-                    fprintf(stderr, "%s open error (errno=)\n", argv[i], errno);
-                    exit(-1);
-                }
-        */
-        printf("xcd(%s)\n", file_name);
-//        fclose(fp);
-    }
-}
+#define BLOCK_SIZE      1024
+#define CHARS_IN_LINE   16
+#define top(x)      ((x/CHARS_IN_LINE) * CHARS_IN_LINE)
+void xcd(FILE *fp);
 
 int main(int argc, char **argv)
 {
@@ -30,7 +16,15 @@ int main(int argc, char **argv)
         xcd(NULL);
     else{
         for (i = 1; i < argc; i++){
-            xcd(argv[i]);
+            printf("xcd(%s)\n", argv[i]);
+
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL){
+                fprintf(stderr, "%s open error (errno=)\n", argv[i], errno);
+                exit(-1);
+            }
+            xcd(fp);
+            fclose(fp);
         }
     }
     return 0;
@@ -41,3 +35,38 @@ err:if (argc == 1)
         fprintf(stderr, "xcd(%s) error\n", argv[i]);
     return -1;
 }
+
+/* 
+ * xcd --- hexa + char dump
+ *      01234567890123456789012345678901234567890123456789012345678901234567890123456789
+ *      xxxx  00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F    abcdefghijklmnop
+ */
+void xcd(FILE *fp)
+{
+    int pos = 0;
+    int n = 0;
+    char c;
+    char abf[8];    
+    char xbf[3*CHARS_IN_LINE+1];
+    char cbf[CHARS_IN_LINE+1];
+
+    while((c = fgetc(fp)) != EOF){
+        if (n == 0){
+            memset(xbf, 0x20, sizeof(xbf));     /* バッファをスペースで初期化 */
+            memset(cbf, 0x20, sizeof(cbf));
+        }
+        sprintf(&xbf[3*n+1], "%02x ", c);
+        sprintf(&cbf[n], "%c", isprint(c)? c: '.');
+
+        n++;
+        if (n >= CHARS_IN_LINE){
+            printf("%04X  %-48s    %-16s\n", top(pos), xbf, cbf);
+            n = 0;
+        }
+        pos++;
+    }
+    if (n != 0)
+        printf("%04X  %-48s    %-16s\n", top(pos), xbf, cbf);
+    return;
+}
+
