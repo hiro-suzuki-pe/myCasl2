9d747afe9e6f75a02fbebdbe4c654bb536e8fd1a
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-08-29 13:06:55 +0900

    hexa dump完成

--- xcd/xcd.c
+++ xcd/xcd.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     int i;
 
     if (argc == 1) 
-        xcd(NULL);
+        xcd(stdin);
     else{
         for (i = 1; i < argc; i++){
             printf("xcd(%s)\n", argv[i]);
@@ -45,28 +45,40 @@ void xcd(FILE *fp)
 {
     int pos = 0;
     int n = 0;
+    int skip = 0;
     char c;
     char abf[8];    
     char xbf[3*CHARS_IN_LINE+1];
+    char xbf_prev[3*CHARS_IN_LINE+1];
     char cbf[CHARS_IN_LINE+1];
+
+    memset(xbf_prev, 0, sizeof(xbf_prev));
 
     while((c = fgetc(fp)) != EOF){
         if (n == 0){
             memset(xbf, 0x20, sizeof(xbf));     /* バッファをスペースで初期化 */
             memset(cbf, 0x20, sizeof(cbf));
         }
-        sprintf(&xbf[3*n+1], "%02x ", c);
+        sprintf(&xbf[3*n], "%02x ", c);
         sprintf(&cbf[n], "%c", isprint(c)? c: '.');
 
         n++;
         if (n >= CHARS_IN_LINE){
-            printf("%04X  %-48s    %-16s\n", top(pos), xbf, cbf);
+            if (strcmp(xbf, xbf_prev) != 0){
+                printf("%04X:  %-48s    %-16s\n", top(pos), xbf, cbf);
+                memcpy(xbf_prev, xbf, sizeof(xbf));
+                skip = 0;
+            }
+            else if (strcmp(xbf, xbf_prev) == 0 && skip == 0){
+                printf("\n%-7s* same value *\n\n", "");
+                skip = 1;
+            }
             n = 0;
         }
         pos++;
     }
-    if (n != 0)
-        printf("%04X  %-48s    %-16s\n", top(pos), xbf, cbf);
+    if (n != 0 || skip == 1)
+        printf("%04X:  %-48s    %-16s\n", top(pos), xbf, cbf);
     return;
 }
 
