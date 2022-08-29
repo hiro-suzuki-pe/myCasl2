edff352d7a286e001c6fcc2aa72bafbc6302ce12
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-08-30 06:13:27 +0900

    xcd完成。スキップも実装

--- xcd/xcd.c
+++ xcd/xcd.c
@@ -92,23 +92,33 @@ void xcd(int fd)
     int pos = 0;
     int i;
     int n;
+    int skip = 0;
     char rbf[CHARS_IN_LINE];
     char xbf[3*CHARS_IN_LINE+1];
+    char xbf_prev[3*CHARS_IN_LINE+1];
     char cbf[CHARS_IN_LINE+1];
 
     n = read(fd, rbf, CHARS_IN_LINE);
+    strcpy(xbf_prev, "");
     while(n == CHARS_IN_LINE){
         for (i = 0; i < CHARS_IN_LINE; i++){
             sprintf(&xbf[3*i], "%02X ", rbf[i]);
             sprintf(&cbf[i], "%c", isprint(rbf[i])? rbf[i]: '.');
         }
-        printf("%04X[%2d]  %-48s    %-16s\n", top(pos), n, xbf, cbf);
-
+        if (strcmp(xbf, xbf_prev) != 0){
+            printf("%04X:  %-48s    %-16s\n", top(pos), xbf, cbf);
+            strcpy(xbf_prev, xbf);
+            skip = 0;
+        }
+        else if (skip == 0){
+            printf("       *\n");
+            skip = 1;
+        }
         pos += CHARS_IN_LINE;
-
         n = read(fd, rbf, CHARS_IN_LINE);
     }
 
+    if (skip == 1) n = CHARS_IN_LINE;
     if (n > 0){
         for (i = 0; i < n; i++){
             sprintf(&xbf[3*i+1], "%02X ", rbf[i]);
@@ -118,7 +128,7 @@ void xcd(int fd)
             sprintf(&xbf[3*i+1], "   ");
             sprintf(&cbf[i], " ");
         }
-        printf("%04X[%2d]  %-48s    %-16s\n", top(pos), n, xbf, cbf);
+        printf("%04X:  %-48s    %-16s\n", top(pos), xbf, cbf);
     }
     return;
 }
