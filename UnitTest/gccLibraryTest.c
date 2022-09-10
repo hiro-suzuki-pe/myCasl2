d80628d64b6203cf4825117624d4fc224a279243
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-09-11 06:36:24 +0900

    fctl (open, close, read, write) tested.

--- UnitTest/gccLibraryTest.c
+++ UnitTest/gccLibraryTest.c
@@ -1,4 +1,11 @@
-//#include "unity.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 #include "unity_fixture.h"
 
 TEST_GROUP(fctl);
@@ -16,6 +23,109 @@ TEST_TEAR_DOWN(fctl)
 {
 }
 
-TEST(fctl, NoTest)
+TEST(fctl, open_RDONLY)
 {
+  int fd =  open("AllTests.exe", O_RDONLY);
+  if (fd == -1)
+    printf("open(%d): %s\n", errno, strerror(errno));
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+}
+
+TEST(fctl, open_WRONLY)
+{
+  int fd =  open("Dummy.dat", O_WRONLY);
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+
+  fd =  open("Dummy.dat", O_WRONLY);
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+}
+
+TEST(fctl, open_CREAT)
+{
+  int fd =  open("test_creat.dat", O_CREAT);
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+  
+  fd =  open("test_creat.dat", O_RDONLY);
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+  
+  fd =  open("test_creat.dat", O_WRONLY);
+  TEST_ASSERT_EQUAL(3,fd);
+  close(fd);
+}
+
+TEST(fctl, writeAndReadSmall)
+{
+  int fd =  open("writeAndReadSmall.dat", O_WRONLY|O_CREAT|O_BINARY, S_IRWXU|S_IRWXU|S_IRWXO);
+  char bf[] = "Small file.\n";
+  int n = write(fd, bf, strlen(bf));
+  TEST_ASSERT_EQUAL(strlen(bf), n);
+  close(fd);
+
+  char rbf[100];
+  fd =  open("writeAndReadSmall.dat", O_RDONLY|O_BINARY);
+  n = read(fd, rbf, strlen(bf));
+  TEST_ASSERT_EQUAL(strlen(bf), n);
+  rbf[n] = '\0';
+  TEST_ASSERT_EQUAL_STRING(bf, rbf);
+  close(fd);
+}
+TEST(fctl, writeAndReadShort)
+{
+  union{
+    char c[1024];
+    short s[512];
+  } u;
+  strcpy(u.c, "1234567890");
+  int fd =  open("writeAndReadShort.dat", O_WRONLY|O_CREAT|O_BINARY, S_IRWXU|S_IRWXU|S_IRWXO);
+  int n = write(fd, u.s, strlen(u.c));
+  TEST_ASSERT_EQUAL(strlen(u.c), n);
+  close(fd);
+
+  char cb[1024];
+  fd =  open("writeAndReadShort.dat", O_RDONLY|O_BINARY);
+  n = read(fd, cb, strlen(u.c));
+  TEST_ASSERT_EQUAL(strlen(u.c), n);
+  cb[n] = '\0';
+  TEST_ASSERT_EQUAL_STRING(u.c, cb);
+  TEST_ASSERT_EQUAL(0x3231, u.s[0]);
+  close(fd);
+}
+TEST(fctl, writeCharAndReadShort)
+{
+  int fd =  open("writeAndReadSmall.dat", O_WRONLY|O_CREAT|O_BINARY, S_IRWXU|S_IRWXU|S_IRWXO);
+  char bf[] = "Small file.\n";
+  int n = write(fd, bf, strlen(bf));
+  TEST_ASSERT_EQUAL(strlen(bf), n);
+  close(fd);
+
+  char rbf[100];
+  fd =  open("writeAndReadSmall.dat", O_RDONLY|O_BINARY);
+  n = read(fd, rbf, strlen(bf));
+  TEST_ASSERT_EQUAL(strlen(bf), n);
+  rbf[n] = '\0';
+  TEST_ASSERT_EQUAL_STRING(bf, rbf);
+  close(fd);
+}
+TEST(fctl, writeShortAndReadChar)
+{
+  union{
+    char c[1024];
+    short s[512];
+  } u;
+  u.s[0] = 0x3132; u.s[1] = 0x3334;
+  int fd =  open("writeShortAndReadChar.dat", O_WRONLY|O_CREAT|O_BINARY, S_IRWXU|S_IRWXU|S_IRWXO);
+  int n = write(fd, u.c, 4);
+  close(fd);
+
+  char cb[1024];
+  fd =  open("writeShortAndReadChar.dat", O_RDONLY|O_BINARY);
+  n = read(fd, u.c, 4);
+    u.c[4] = '\0';
+  TEST_ASSERT_EQUAL_STRING("2143", u.c);
+  close(fd);
 }
--- UnitTest/gccLibraryTestRunner.c
+++ UnitTest/gccLibraryTestRunner.c
@@ -3,5 +3,10 @@
 
 TEST_GROUP_RUNNER(fctl)
 {
-  RUN_TEST_CASE(fctl, NoTest);
+  RUN_TEST_CASE(fctl, open_RDONLY);
+  RUN_TEST_CASE(fctl, open_WRONLY);
+  RUN_TEST_CASE(fctl, open_CREAT);
+  RUN_TEST_CASE(fctl, writeAndReadSmall);
+  RUN_TEST_CASE(fctl, writeCharAndReadShort);
+  RUN_TEST_CASE(fctl, writeShortAndReadChar);
 }
